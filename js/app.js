58498fef291469120ca64801444bf31ba0273083
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2017-03-16 22:20:33 +0500

    Post-Pre (Incr & Decr) and For Loop

--- js/app.js
+++ js/app.js
@@ -16,4 +16,27 @@ weight = weight + 5;
 alert(weight);
 
 var modulus = 9;
-alert("It shows remainder of when 2 divides 9 = " + modulus%2); 
+alert("It shows remainder of when 2 divides 9 = " + modulus%2);
+
+var number = 0;
+var num;
+
+for(num = number++; num<10; num++)
+{
+    alert(num);
+}
+
+for(num = number; num<10; ++num)
+{
+    alert(num);
+}
+
+for(num = 10; num>0; num--)
+{
+    alert(num);
+}
+
+for(num = 10; num>0; --num)
+{
+    alert(num);
+}
