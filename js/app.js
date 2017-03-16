ba64cd72121873adeafed23e1c0833720551e80b
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2017-03-16 19:21:58 +0500

    Playing with Variable

---
+++ js/app.js
@@ -0,0 +1,10 @@
+alert ("Hello World!");
+window.alert("This is window.alert function");
+var name = "Abdul Moiz Lakhani";
+var nationality = "Pakistani";
+
+window.alert(name);
+alert(nationality);
+
+name = "Ghulam Rasool Lakhani";
+nationality = "American";
